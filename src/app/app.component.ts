a847d073a1cba692f1f3d5885a75406421ee1808
John Firebaugh <j.s.firebaugh@gmail.com>
2020-12-10 09:55:41 -0500

    mods for bs5_beta1 from bs5_alpha3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,7 +38,8 @@ export class AppComponent implements OnInit {
       data => { 
         function sortByKey(array, key) {
           return array.sort(function(a, b) {
-            var x = a[key]; var y = b[key];
+            var x = a[key]; 
+            var y = b[key];
             return ((x < y) ? -1 : ((x > y) ? 1 : 0));
           });
         }
